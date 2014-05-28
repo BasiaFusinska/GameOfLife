31bb541d1520534c8d5b58cc5674439cb2140d4d
BasiaFusinska <barbara.fusinska@gmail.com>
2014-05-28 15:21:07 +0200

    moving CountNeighbours to Board

--- GameOfLife/GameOfLife/Board.cs
+++ GameOfLife/GameOfLife/Board.cs
@@ -9,10 +9,14 @@ namespace GameOfLife
 {
     public class Board
     {
+        private readonly int _length;
+        private readonly int _width;
         private readonly bool[,] _board;
 
         public Board(IEnumerable<Point> points, int length, int width)
         {
+            _length = length;
+            _width = width;
             _board = new bool[length, width];
 
             foreach (var point in points)
@@ -25,6 +29,21 @@ namespace GameOfLife
 
                 _board[x, y] = true;
             }
+        }
+
+        public int CountNeighbours(int x, int y)
+        {
+            var neighboursCount = 0;
+
+            for (var i = Math.Max(0, x - 1); i <= Math.Min(_length - 1, x + 1); i++)
+            {
+                for (var j = Math.Max(0, y - 1); j <= Math.Min(_width - 1, y + 1); j++)
+                {
+                    neighboursCount += IsAlive(i, j) ? 1 : 0;
+                }
+            }
+
+            return neighboursCount - (IsAlive(x, y) ? 1 : 0);
         }
 
         public bool IsAlive(int x, int y)
--- GameOfLife/GameOfLife/Game.cs
+++ GameOfLife/GameOfLife/Game.cs
@@ -35,25 +35,11 @@ namespace GameOfLife
         private bool ApplyRule(int x, int y)
         {
             var value = _board.IsAlive(x, y);
-            var neighboursCount = CountNeighbours(x, y);
+            var neighboursCount = _board.CountNeighbours(x, y);
 
             return value ? neighboursCount > 1 && neighboursCount < 4 : neighboursCount == 3;
         }
 
-        private int CountNeighbours(int x, int y)
-        {
-            var neighboursCount = 0;
-
-            for (var i = Math.Max(0, x-1); i <= Math.Min(_length - 1, x+1); i++)
-            {
-                for (var j = Math.Max(0, y-1); j <= Math.Min(_width - 1, y+1); j++)
-                {
-                    neighboursCount += _board.IsAlive(i, j) ? 1 : 0;
-                }
-            }
-
-            return neighboursCount - (_board.IsAlive(x, y) ? 1 : 0);
-        }
 
         public bool IsAlive(int x, int y)
         {
